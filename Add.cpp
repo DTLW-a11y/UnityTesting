91ced6e613cfc5b0683992db11925022789aa17b
DTLW-a11y <3216341428@qq.com>
2025-11-08 20:41:40 +0800

    A

--- Add.cpp
+++ Add.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int AddInt(int a, int b) {
+	cout << "This is Int adding." << endl;
+	a = a + b;
+	a = a - b;
 	return a + b;
 }
 
 int main() {
-	cout << AddInt(1, 1);
+	cout << AddInt(1, 1) <<endl;
 	return 0;
 }
